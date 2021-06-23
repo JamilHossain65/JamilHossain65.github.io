aa533a76debca4593f4a3dc749d2078f3a6f2e9e
JamilHossain65 <45550369+JamilHossain65@users.noreply.github.com>
2021-06-23 04:31:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
  
-app = Flask(__name__)
+app = Flask(__name__,template_folder='template')
  
 @app.route('/')
 def index():
@@ -8,10 +8,11 @@ def index():
  
 @app.route('/square/', methods=['POST'])
 def square():
+    print(request.form)
     num = float(request.form.get('number', 0))
     square = num ** 2
     data = {'square': square}
     data = jsonify(data)
     return data
  
- app.run(debug=True)
+app.run()
